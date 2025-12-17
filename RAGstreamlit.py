97571033d91c8f660075382698705a315b0dbf45
DHANAVARATHAN P <dhanavarathanperumal@gmail.com>
2025-12-17 21:14:00 +0530

    Fix import path for RecursiveCharacterTextSplitter

--- RAGstreamlit.py
+++ RAGstreamlit.py
@@ -6,7 +6,7 @@ from typing import List
 import streamlit as st
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.chains import RetrievalQA
